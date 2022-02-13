d31bddd77b1fe4282daf4b2b9bf2631cb8f24823
yuanhong.zeng <>
2022-02-13 14:58:15 -0800

    Added some ui

---
+++ server/controllers/post.js
@@ -0,0 +1,16 @@
+// handlers
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  }
+  catch(error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const createPost = (req, res) => {
+  res.send("Post Created");
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import postRoutes from '../routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes); // localhost:5000/posts
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Use Mongodb cloud atlas
+const CONNECTION_URL = 'mongodb+srv://danny_zyh:bruin-on-sale@cluster0.dhu6d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(
+  () => app.listen(PORT, 
+    () => console.log(`Server running on ${PORT}`))
+).catch( // connection not successful
+  (error) => console.log(error.message)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

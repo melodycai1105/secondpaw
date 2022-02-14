a17e8903172d31d6584d3c492689b45e22fb50e4
yuanhong.zeng <>
2022-02-13 16:14:38 -0800

    added app.js functionality and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Form from './components/Form/Form.js';
-import Posts from './components/Posts/Posts.js';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return ( 

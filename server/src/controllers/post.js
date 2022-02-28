8791f44987428fb70db89041fc55621aa1ba9140
yuanhong.zeng <>
2022-02-28 11:10:59 -0800

    minor bug fix in controllers and postMessage

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -29,7 +29,7 @@ export const getPostsBySearch = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
 
     try {
         await newPost.save();
--- server/src/models/postMessage.js
+++ server/src/models/postMessage.js
@@ -4,6 +4,7 @@ const postSchema = mongoose.Schema({
   title: String,
   message: String,
   creator: String,
+  name: String,
   tags: [String],
   selectedFile: String,
   likes: {

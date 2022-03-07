fa0d6422150e92957cf57f67ff5bd5fb5562c05e
alexiazh <alexiazhou726@ucla.edu>
2022-03-07 01:01:40 -0800

    Fix bugs in post details

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -26,8 +26,8 @@ import TagIcon from '@mui/icons-material/Tag';
 import AddReactionIcon from '@mui/icons-material/AddReaction';
 
 const PostDetails = () => {
-  //const user = JSON.parse(localStorage.getItem('profile'));
-  const { user } = useSelector((state) => state.user);
+  const user = JSON.parse(localStorage.getItem('profile'));
+  // const { user } = useSelector((state) => state.user);
   const { post, posts, isLoading } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -146,10 +146,10 @@ const PostDetails = () => {
           </div>
           <div className={classes.section} style={{ display: 'flex', margin:'15px 0px 0px 0px'}}>
             <Typography gutterBottom variant="subtitle1" style={{margin:'5px 0px 0px 0px'}}>Seller:</Typography>
-            <Chip color='secondary' avatar={<Avatar src={user.profile_pic || default_profile_pic} />} 
-                          //<img className={classes.media} src={user.profile_pic || default_profile_pic} />
+            {/* <Chip color='secondary' avatar={<Avatar src={user.profile_pic || default_profile_pic} />} 
+              //<img className={classes.media} src={user.profile_pic || default_profile_pic} />
               label= {post.name} onClick={toUser} style={{margin:'5px 0px 0px 5px'}}>
-            </Chip>
+            </Chip> */}
           </div>
           <Typography gutterBottom variant="subtitle1" component="p" style={{margin:'15px 0px 15px 0px'}}>Descriptions: {post.message}</Typography>
           {/* <Typography gutterBottom variant="h6" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography> */}
